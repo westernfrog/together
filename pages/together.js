15e035797bf0342f0542fb048fb8a306b9b1100a
Aman Singh <95922370+westernfrog@users.noreply.github.com>
2023-04-03 11:57:54 +0530

    Updated

--- pages/together.js
+++ pages/together.js
@@ -1,12 +1,13 @@
 import { GetUserName } from "@/components/GetUserName";
 import CreatePost from "@/components/CreatePost";
 import Suggested from "@/components/Suggested";
-import { Button } from "@nextui-org/react";
-import Link from "next/link";
+import { Button, Grid } from "@nextui-org/react";
 import Post from "@/components/Post";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Together() {
+  const router = useRouter();
   const userData = GetUserName();
   const [posts, setPosts] = useState([]);
   const [numPosts, setNumPosts] = useState(0);
@@ -63,59 +64,66 @@ export default function Together() {
     }
   };
 
+  const handleLogin = async () => {
+    router.push({
+      pathname: "/login",
+    });
+  };
+
+  const handleSignUp = async () => {
+    router.push({
+      pathname: "/signup",
+    });
+  };
+
   return (
     <>
       {!userData && (
-        <div className="col-md-6 mx-auto my-5 py-5">
-          <p className="display-1">
-            Create an account to interact with users 😭🤷‍♂️
-          </p>
-          <div className="d-flex align-items-center gap-3">
-            <Button auto flat color={"secondary"} className="text-dm">
-              <Link
-                href="/login"
-                className="text-decoration-none"
-                style={{ color: "inherit" }}
-              >
-                Log in
-              </Link>
-            </Button>
-            <Button auto flat color={"secondary"} className="text-dm">
-              <Link
-                href="/signup"
-                className="text-decoration-none"
-                style={{ color: "inherit" }}
-              >
-                Sign up
-              </Link>
-            </Button>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+          <div className="col-md-6">
+            <p className="display-1">
+              Create an account to interact with users 😭🤷‍♂️
+            </p>
+            <div className="d-flex align-items-center gap-3 text-dm">
+              <Grid>
+                <Button color="secondary" auto flat onPress={handleSignUp}>
+                  Register
+                </Button>
+              </Grid>
+              <Grid>
+                <Button color="secondary" auto flat onPress={handleLogin}>
+                  Login
+                </Button>
+              </Grid>
+            </div>
           </div>
         </div>
       )}
       <div className="row d-flex justify-content-between">
-        <div className="col-md-8 mt-2 mt-lg-4">
-          {userData && (
+        {userData && (
+          <div className="col-md-8 mt-2 mt-lg-4">
             <CreatePost
               name={userData?.name?.split(" ")[0]}
               setNumPosts={setNumPosts}
               username={username}
               numPosts={numPosts + 1}
             />
-          )}
-          {posts.map((post, index) => (
-            <Post
-              key={index}
-              index={index}
-              name={post.author}
-              username={post.username}
-              text={post.text}
-              id={post._id}
-              likes={post.likes}
-              handleLike={handleLike}
-              createdAt={post.createdAt}
-            />
-          ))}
-        </div>
+
+            {posts.map((post, index) => (
+              <Post
+                key={index}
+                index={index}
+                name={post.author}
+                username={post.username}
+                text={post.text}
+                id={post._id}
+                likes={post.likes}
+                handleLike={handleLike}
+                createdAt={post.createdAt}
+              />
+            ))}
+          </div>
+        )}
         <div className="col-md-4">
           <div className="position-sticky" style={{ top: "8rem" }}>
             {userData && <Suggested username={userData.username} />}
